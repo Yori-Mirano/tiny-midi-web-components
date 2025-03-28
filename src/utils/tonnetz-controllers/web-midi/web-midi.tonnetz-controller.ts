a8ca3bb9bad4c8151faf83788f8fee8b659862c2
Yori Mirano <yori.mirano@gmail.com>
2025-03-28 16:39:10 +0100

    feat: adds unlisten method to the webmidi tonnetz controller

--- src/utils/tonnetz-controllers/web-midi/web-midi.tonnetz-controller.ts
+++ src/utils/tonnetz-controllers/web-midi/web-midi.tonnetz-controller.ts
@@ -1,11 +1,9 @@
-import { Input } from "webmidi";
+import { ControlChangeMessageEvent, Input, NoteMessageEvent } from "webmidi";
 import { Components } from "../../../components";
 import TinyTonnetz = Components.TinyTonnetz;
 import { NoteKey, NoteState, SEMI_TONE_COUNT } from "../../models";
 
 export class WebMidiTonnetzController {
-
-  webMidiInput?: Input;
 
   sustainThreshold = 43; // from 0 to 127
 
@@ -14,51 +12,59 @@ export class WebMidiTonnetzController {
   constructor(public tonnetz: TinyTonnetz) {}
 
   listen(webMidiInput: Input) {
-    this.webMidiInput = webMidiInput;
+    webMidiInput.addListener("noteon", this.onNoteOn);
+    webMidiInput.addListener('noteoff', this.onNoteOff);
+    webMidiInput.addListener('controlchange', this.onControlchange);
+  }
 
-    this.webMidiInput.addListener("noteon", e => {
-      let notes: Array<NoteKey> = this.tonnetz.activeNotes?.[e.note.number % SEMI_TONE_COUNT];
+  unlisten(webMidiInput: Input) {
+    webMidiInput.removeListener("noteon", this.onNoteOn);
+    webMidiInput.removeListener('noteoff', this.onNoteOff);
+    webMidiInput.removeListener('controlchange', this.onControlchange);
+  }
 
-      if (!notes) {
-        notes = [];
-        this.tonnetz.activeNotes[e.note.number % SEMI_TONE_COUNT] = notes;
+  onNoteOn = (event: NoteMessageEvent) => {
+    let notes: Array<NoteKey> = this.tonnetz.activeNotes?.[event.note.number % SEMI_TONE_COUNT];
+
+    if (!notes) {
+      notes = [];
+      this.tonnetz.activeNotes[event.note.number % SEMI_TONE_COUNT] = notes;
+    }
+
+    notes.push({ state: NoteState.PRESSED });
+
+    this.tonnetz.activeNotes = { ...this.tonnetz.activeNotes }
+  }
+
+  onNoteOff = (event: NoteMessageEvent) => {
+    let notes: Array<NoteKey> = this.tonnetz.activeNotes?.[event.note.number % SEMI_TONE_COUNT];
+
+    if (notes.length) {
+      if (this.sustained) {
+        const note = notes.find(note => note.state === NoteState.PRESSED);
+        if (note) {
+          note.state = NoteState.SUSTAINED;
+        }
+      } else {
+        notes.shift();
       }
 
-      notes.push({ state: NoteState.PRESSED });
-
       this.tonnetz.activeNotes = { ...this.tonnetz.activeNotes }
-    });
+    }
+  }
 
-    this.webMidiInput.addListener("noteoff", e => {
-      let notes: Array<NoteKey> = this.tonnetz.activeNotes?.[e.note.number % SEMI_TONE_COUNT];
+  onControlchange = (event: ControlChangeMessageEvent) => {
+    if (event.subtype === 'damperpedal') {
+      this.sustained = event.rawValue > this.sustainThreshold;
 
-      if (notes.length) {
-        if (this.sustained) {
-          const note = notes.find(note => note.state === NoteState.PRESSED);
-          if (note) {
-            note.state = NoteState.SUSTAINED;
-          }
-        } else {
-          notes.shift();
+      if (!this.sustained && this.tonnetz.activeNotes) {
+        for (let notesKey in this.tonnetz.activeNotes) {
+          let notes: Array<NoteKey> = this.tonnetz.activeNotes[notesKey];
+          this.tonnetz.activeNotes[notesKey] = notes.filter(note => note.state !== NoteState.SUSTAINED);
         }
 
         this.tonnetz.activeNotes = { ...this.tonnetz.activeNotes }
       }
-    });
-
-    this.webMidiInput.addListener("controlchange", e => {
-      if (e.subtype === 'damperpedal') {
-        this.sustained = e.rawValue > this.sustainThreshold;
-
-        if (!this.sustained && this.tonnetz.activeNotes) {
-          for (let notesKey in this.tonnetz.activeNotes) {
-            let notes: Array<NoteKey> = this.tonnetz.activeNotes[notesKey];
-            this.tonnetz.activeNotes[notesKey] = notes.filter(note => note.state !== NoteState.SUSTAINED);
-          }
-
-          this.tonnetz.activeNotes = { ...this.tonnetz.activeNotes }
-        }
-      }
-    });
+    }
   }
 }
